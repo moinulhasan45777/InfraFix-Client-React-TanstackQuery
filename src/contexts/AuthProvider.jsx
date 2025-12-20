18d227aaa74cd383571839b351654e8ae875e7bf
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-12-20 23:18:17 +0600

    Designed Register Page and implemented react hook forms

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase.init";
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = {
+    registerUser,
+    signInUser,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
--- src/pages/authentication/register/Register.jsx
+++ src/pages/authentication/register/Register.jsx
@@ -1,7 +1,109 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
 
 const Register = () => {
-  return <div>Register</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1 className="text-4xl font-black text-center mb-8 text-primary-content">
+        Register
+      </h1>
+      <form onSubmit={handleSubmit(handleRegistration)} className="w-80 ">
+        <fieldset className="fieldset">
+          <label className="label text-primary-content">Full Name</label>
+          <input
+            type="text"
+            {...register("name", { required: true })}
+            className="input"
+            placeholder="Full Name"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Full Name is required!</p>
+          )}
+          <label className="label text-primary-content">Photo</label>
+          <input type="file" className="file-input" />
+          <label className="label text-primary-content">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input "
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required!</p>
+          )}
+          <label className="label text-primary-content">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required!</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">Password must be 6 characters long!</p>
+          )}
+          <button className="btn btn-secondary mt-4 mb-2">Register</button>
+          <div className="flex items-center gap-2">
+            <div className="h-px w-full bg-primary"></div>
+            <p className="font-bold">OR</p>
+            <div className="h-px w-full bg-primary"></div>
+          </div>
+          <button className="btn bg-base-200 text-black border-[#e5e5e5]">
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+            Login with Google
+          </button>
+          <p className="text-center text-sm">
+            Already have an account?{" "}
+            <Link
+              to="/auth/login"
+              className="ml-1 text-secondary underline hover:no-underline underline-offset-3"
+            >
+              Login
+            </Link>
+          </p>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
